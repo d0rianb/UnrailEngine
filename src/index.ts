8945212b064a4f569cefdafd85e5f3c65a0015cb
d0rianb <dorian.beauchesne@icloud.com>
2021-03-24 15:20:04 +0100

    Rework exports

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 import { version } from '../package.json'
 
-export { Game } from './core/game'
+// Core exports
 export * from './core/math'
+export { Game } from './core/game'
 export { Env as GameEnvironement } from './core/env'
 export { GameObject, PlayerObject } from './core/objects'
 export { getWindowDimensions, createCanvas } from './core/geometry'
 export { Particle, ParticuleGenerator } from './core/particles'
+export { Grid, Cell } from './core/grid'
+
+
+// Modules exports
+export * from './events'
+export * from './render'
+export * from './config'
 
 export const VERSION = version
