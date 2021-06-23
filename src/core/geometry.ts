da41f6e7e529bc95fea289559c9e78e24083b4e5
d0rianb <dorian.beauchesne@icloud.com>
2021-06-23 14:08:59 +0200

    add setCanvasDimension method

--- src/core/geometry.ts
+++ src/core/geometry.ts
@@ -11,6 +11,13 @@ function getCanvasDimensions(canvas: HTMLCanvasElement): SizeObject {
     return { width: canvas.clientWidth || canvas.width, height: canvas.clientHeight || canvas.height }
 }
 
+function setCanvasDimensions(canvas: HTMLCanvasElement, width: number, height: number, pixelRatio?: number): void {
+    canvas.width = width * (pixelRatio || window.devicePixelRatio || 1)
+    canvas.height = height * (pixelRatio || window.devicePixelRatio || 1)
+    canvas.style.width = width + 'px'
+    canvas.style.height = height + 'px'
+}
+
 function createCanvas(w: number, h: number, ratio?: number, preventRightClick?: boolean): HTMLCanvasElement {
     const canvas: HTMLCanvasElement = document.createElement('canvas')
     adaptCanvasToDevicePixelRatio(canvas, w, h, ratio)
@@ -24,10 +31,7 @@ function adaptCanvasToDevicePixelRatio(canvas: HTMLCanvasElement, width?: number
     const pixelRatio: number = ratio || window.devicePixelRatio || 1
     let w: number = width || getCanvasDimensions(canvas).width
     let h: number = height || getCanvasDimensions(canvas).height
-    canvas.width = w * pixelRatio
-    canvas.height = h * pixelRatio
-    canvas.style.width = w + 'px'
-    canvas.style.height = h + 'px'
+    setCanvasDimensions(canvas, w, h, pixelRatio)
     if (pixelRatio != 1) { // The context must be clear for transfering an OffscreenCanvas
         canvas.getContext('2d')!.setTransform(pixelRatio, 0, 0, pixelRatio, 0, 0)
     }
@@ -42,4 +46,4 @@ function insertCanvas(canvas: HTMLCanvasElement, el: string): void {
     })
 }
 
-export { getWindowDimensions, getCanvasDimensions, createCanvas, adaptCanvasToDevicePixelRatio, insertCanvas }
+export { getWindowDimensions, getCanvasDimensions, setCanvasDimensions, createCanvas, adaptCanvasToDevicePixelRatio, insertCanvas }
